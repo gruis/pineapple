e11727ec194c353d6640259e083a858d501f7010
Caleb Crane <github@simulacre.org>
2023-06-28 18:33:11 +0900

    :bug:

--- lib/gruis/pineapple/trunk.rb
+++ lib/gruis/pineapple/trunk.rb
@@ -2,9 +2,10 @@ module Gruis
   module Pineapple
     # Trunk holds all possible Subjects gathered from various sources, e.g., Wanikani
     class Trunk
+
       def initialize(subjects)
         # text indexed
-        @subjects = {}
+        @subjects = OpenStruct.new({kanji: {}, radical: {}, vocabulary: {}})
         reset!
         add(*subjects)
       end
@@ -15,13 +16,21 @@ module Gruis
 
       def add(*subs)
         subs.each do |sub|
-          @subjects[sub.to_s] = sub unless @subjects[sub.to_s]
+          @subjects[sub.type][sub.to_s] = sub unless @subjects[sub.type][sub.to_s]
         end
         index!
       end
 
+      def radicals
+        @radicals ||= @subjects.radical.values
+      end
+
       def kanji
-        @kanji ||= @subjects.values.select(&:kanji?)
+        @kanji ||= @subjects.kanji.values
+      end
+
+      def vocab
+        @vocab ||= @subjects.vocabulary.values
       end
 
       def kanji_vocab
@@ -33,9 +42,6 @@ module Gruis
           .select(&:is_compound?)
       end
 
-      def vocab
-        @vocab ||= @subjects.values.select(&:vocabulary?)
-      end
       def kanjis_for(subject)
         subject.component_subject_ids.map { |sid| subject_by_id(sid) }
       end
@@ -54,11 +60,14 @@ module Gruis
         @id_index[id]
       end
 
+      def subjects
+        radicals + kanji + vocab
+      end
+
       private
 
       def reset!
         @id_index    = {}
-        @kanji_index = {}
         @comp_index  = {}
         @vocab_index = {}
         @kanji_comp_by_kanji = Hash.new { |h,k| h[k] = {} }
@@ -67,19 +76,14 @@ module Gruis
 
       def index!
         reset!
-        @subjects.values.each do |s| 
-          @id_index[s.id] = s 
-          @kanji_index[s.to_s] = s if s.kanji?
-        end
-        @subjects.values.each do |s|
-          if s.vocabulary?
-            kanjis_for(s).each {|k| @comp_by_kanji[k.to_s][s.to_s] = s }
-            if s.is_compound?
-              kanjis_for(s).each {|k| @kanji_comp_by_kanji[k.to_s][s.to_s] = s }
-            end
+        subjects.each { |s| @id_index[s.id] = s }
+        vocab.each do |s|
+          kanjis_for(s).each {|k| @comp_by_kanji[k.to_s][s.to_s] = s }
+          if s.is_compound?
+            kanjis_for(s).each {|k| @kanji_comp_by_kanji[k.to_s][s.to_s] = s }
           end
         end
-        @subjects
+        subjects
       end
     end
   end
